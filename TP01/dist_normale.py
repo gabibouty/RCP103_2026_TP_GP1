0c65e30904e1686a6f7215dfb9daf095a01b01bc
Stude21 <156705768+Stude21@users.noreply.github.com>
2026-04-28 21:34:32 +0200

    Add distribution normal and uniform

---
+++ TP01/dist_normale.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+rng = np.random.default_rng(seed = 1)
+vmin = 0
+vmax = 10000
+
+# Distribution normale
+m = 50 # moyenne
+s = 10 # ecart type
+
+for n in [10, 100 , 1000, 10000]:
+    # Tirer n normales 
+    # https://numpy.org/doc/stable/reference/random/generated/numpy.random.Generator.normal.html
+    vrandom = rng.normal(m, s, size=n)
+    print(f"=== n = {n} ===\n {vrandom}")
+
+    # Tracé de l'histogramme
+    plt.figure(figsize=(10, 6))
+    plt.hist(vrandom, bins=20, edgecolor='black', alpha=0.7)
+    plt.title(f"Histogramme pour n = {n}")
+    plt.xlabel("Valeur")
+    plt.ylabel("Fréquence")
+    plt.xticks(np.linspace(m - 4*s, m + 4*s, 9))
+    # plt.xticks(np.linspace(vmin, vmax, 10))
+    plt.grid(axis='y', alpha=0.75)
+    #plt.show()
+    
+    # sauvegarde
+    plt.savefig(f'dist_norm_histogramme_n_{n}.png', dpi=200, bbox_inches='tight')
+    plt.close() 
+
+
