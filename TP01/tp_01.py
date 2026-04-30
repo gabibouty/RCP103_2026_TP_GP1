6f933839d09fa0c763ee5dbce1c5eb88a89f5f06
gabibouty <gabi_bouty@hotmail.com>
2026-04-30 08:24:21 +0200

    Add tp file, factorize again, and print final pngs

--- TP01/tp_01.py
+++ TP01/tp_01.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-SEED = 1
+# ========================================================
+# CONSTANTS
 
+SEED = 1
 N_VALUES = [10, 100, 1000, 10000]
 
 
-def init_rdm_and_axes():
-    np.random.seed(SEED)
+# ========================================================
+# COMMONS
+
+
+def init_axes():
     _, axes = plt.subplots(
         int(np.sqrt(len(N_VALUES))), int(np.sqrt(len(N_VALUES))), figsize=(12, 10)
     )
@@ -15,39 +20,70 @@ def init_rdm_and_axes():
     return axes
 
 
-def draw_on_axe(axe, x, n, title):
-    axe.hist(x, bins="auto", edgecolor="black", alpha=0.5)
+def draw_on_axe(axe, x, n, title, bins = 'auto'):
+    axe.hist(x, bins=bins, edgecolor="black", alpha=0.5)
     axe.set_title(f"{title} (n={n})")
     axe.set_xlabel("$x$")
     axe.set_ylabel("$F$")
     axe.grid(True, linestyle="--", alpha=0.5)
 
 
-def uniform_real():
-    axes = init_rdm_and_axes()
+# ========================================================
+# DISTRIBUTIONS
+
+
+def uniform_discrete():
+    axes = init_axes()
     for i, n in enumerate(N_VALUES):
+        np.random.seed(SEED)
+        MIN = 0
+        MAX = 20
+        x = np.random.randint(low=MIN, high=MAX + 1, size=n)
+        draw_on_axe(axes[i], x, n, "Uniform Discrete", bins=MAX)
+    plt.tight_layout()
+    plt.savefig("uniform_discrete.png")
+
+
+def uniform_real():
+    axes = init_axes()
+    for i, n in enumerate(N_VALUES):
+        np.random.seed(SEED)
         MIN = 0.0
         MAX = 1.0
         x = np.random.uniform(low=MIN, high=MAX, size=n)
-        draw_on_axe(axes[i], x, n, "Uniform")
+        draw_on_axe(axes[i], x, n, "Uniform Real")
     plt.tight_layout()
     plt.savefig("uniform_real.png")
 
 
 def exponential():
-    axes = init_rdm_and_axes()
+    axes = init_axes()
     for i, n in enumerate(N_VALUES):
+        np.random.seed(SEED)
         AVG = 1
         # More AVG is high, more the distibution decrease quickly
-        x = np.random.exponential(scale=1 / AVG, size=n)
+        x = np.random.exponential(scale=(1 / AVG), size=n)
         draw_on_axe(axes[i], x, n, "Exponential")
     plt.tight_layout()
     plt.savefig("exponential.png")
 
 
-def binomial():
-    axes = init_rdm_and_axes()
+def normal():
+    axes = init_axes()
     for i, n in enumerate(N_VALUES):
+        np.random.seed(SEED)
+        MU_AVG = 0
+        SIGMA_VAR = 1
+        x = np.random.normal(loc=MU_AVG, scale=SIGMA_VAR, size=n)
+        draw_on_axe(axes[i], x, n, "Normal")
+    plt.tight_layout()
+    plt.savefig("normal.png")
+
+
+def binomial():
+    axes = init_axes()
+    for i, n in enumerate(N_VALUES):
+        np.random.seed(SEED)
         TRIES = 10
         P = 0.5
         x = np.random.binomial(p=P, n=TRIES, size=n)
@@ -56,7 +92,11 @@ def binomial():
     plt.savefig("binomial.png")
 
 
+# ========================================================
+# MAIN
 if __name__ == "__main__":
+    uniform_discrete()
     uniform_real()
     exponential()
+    normal()
     binomial()
