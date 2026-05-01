5b3f746724c545a6cdc7d35ed9fc4a6542c17242
gabibouty <gabi_bouty@hotmail.com>
2026-05-01 09:49:56 +0200

    Update main program and prepare latex report

--- TP01/tp_01.py
+++ TP01/tp_01.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 # ========================================================
 # CONSTANTS
@@ -20,12 +21,19 @@ def init_axes():
     return axes
 
 
-def draw_on_axe(axe, x, n, title, bins = 'auto'):
+def draw_on_axe(
+    axe, x, n, title, bins="auto", use_int_x_axes=False, start_at_zero=True
+):
     axe.hist(x, bins=bins, edgecolor="black", alpha=0.5)
     axe.set_title(f"{title} (n={n})")
     axe.set_xlabel("$x$")
     axe.set_ylabel("$F$")
     axe.grid(True, linestyle="--", alpha=0.5)
+    if use_int_x_axes:
+        axe.xaxis.set_major_locator(MultipleLocator(1))
+    if start_at_zero:
+        axe.set_xlim(left=0)
+        axe.set_ylim(bottom=0)
 
 
 # ========================================================
@@ -35,11 +43,19 @@ def draw_on_axe(axe, x, n, title, bins = 'auto'):
 def uniform_discrete():
     axes = init_axes()
     for i, n in enumerate(N_VALUES):
-        np.random.seed(SEED)
         MIN = 0
         MAX = 20
-        x = np.random.randint(low=MIN, high=MAX + 1, size=n)
-        draw_on_axe(axes[i], x, n, "Uniform Discrete", bins=MAX)
+        rng = np.random.default_rng(seed=SEED)
+        x = rng.integers(low=MIN, high=MAX + 1, size=n)
+        draw_on_axe(
+            axes[i],
+            x,
+            n,
+            "Uniform Discrete",
+            bins=np.arange(-0.5, MAX + 1 + 0.5, 1),
+            start_at_zero=False,
+            use_int_x_axes=True,
+        )
     plt.tight_layout()
     plt.savefig("uniform_discrete.png")
 
@@ -47,10 +63,9 @@ def uniform_discrete():
 def uniform_real():
     axes = init_axes()
     for i, n in enumerate(N_VALUES):
-        np.random.seed(SEED)
-        MIN = 0.0
-        MAX = 1.0
-        x = np.random.uniform(low=MIN, high=MAX, size=n)
+        rng = np.random.default_rng(seed=SEED)
+        # By default, rng.uniform return values in range [0.0;1.0] (include)
+        x = rng.uniform(size=n)
         draw_on_axe(axes[i], x, n, "Uniform Real")
     plt.tight_layout()
     plt.savefig("uniform_real.png")
@@ -59,10 +74,10 @@ def uniform_real():
 def exponential():
     axes = init_axes()
     for i, n in enumerate(N_VALUES):
-        np.random.seed(SEED)
         AVG = 1
         # More AVG is high, more the distibution decrease quickly
-        x = np.random.exponential(scale=(1 / AVG), size=n)
+        rng = np.random.default_rng(seed=SEED)
+        x = rng.exponential(scale=(1 / AVG), size=n)
         draw_on_axe(axes[i], x, n, "Exponential")
     plt.tight_layout()
     plt.savefig("exponential.png")
@@ -71,11 +86,11 @@ def exponential():
 def normal():
     axes = init_axes()
     for i, n in enumerate(N_VALUES):
-        np.random.seed(SEED)
         MU_AVG = 0
         SIGMA_VAR = 1
-        x = np.random.normal(loc=MU_AVG, scale=SIGMA_VAR, size=n)
-        draw_on_axe(axes[i], x, n, "Normal")
+        rng = np.random.default_rng(seed=SEED)
+        x = rng.normal(loc=MU_AVG, scale=SIGMA_VAR, size=n)
+        draw_on_axe(axes[i], x, n, "Normal", start_at_zero=False)
     plt.tight_layout()
     plt.savefig("normal.png")
 
@@ -83,11 +98,19 @@ def normal():
 def binomial():
     axes = init_axes()
     for i, n in enumerate(N_VALUES):
-        np.random.seed(SEED)
         TRIES = 10
-        P = 0.5
-        x = np.random.binomial(p=P, n=TRIES, size=n)
-        draw_on_axe(axes[i], x, n, "Binomial")
+        P = 0.50
+        rng = np.random.default_rng(seed=SEED)
+        x = rng.binomial(p=P, n=TRIES, size=n)
+        draw_on_axe(
+            axes[i],
+            x,
+            n,
+            "Binomial",
+            bins=np.arange(-0.5, TRIES + 1 + 0.5, 1),
+            use_int_x_axes=True,
+            start_at_zero=False,
+        )
     plt.tight_layout()
     plt.savefig("binomial.png")
 
