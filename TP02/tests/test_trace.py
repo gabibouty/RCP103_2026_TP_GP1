5bbeff55c4fbede66147fae5bda62467680b338e
Julien <julien@local>
2026-05-06 15:37:40 +0200

    add some tests

--- TP02/tests/test_trace.py
+++ TP02/tests/test_trace.py
@@ -2,7 +2,7 @@ from sources.events_and_messages import Message, Event, EventType
 from sources.trace import get_time_and_node
 
 
-def test_get_time_and_node():
+def test_get_time_and_node_event_SEND_MSG():
     message = Message(1, 1, 2)
     message.set_message_send_time(0.0)
     message.set_message_arrival_time(1.1)
@@ -10,3 +10,24 @@ def test_get_time_and_node():
 
     event = Event(1, EventType.SEND_MSG, 0.0, message)
     assert get_time_and_node(event) == (1, 0.0)
+
+def test_get_time_and_node_event_RECV_MSG():
+    message = Message(1, 1, 2)
+    message.set_message_send_time(0.0)
+    message.set_message_arrival_time(1.1)
+    message.set_message_server_time(2.6)
+
+    event = Event(1, EventType.RECV_MSG, 0.0, message)
+    assert get_time_and_node(event) == (2, 1.1)
+
+def test_get_time_and_node_event_MSG_DEPT():
+    message = Message(1, 1, 2)
+    message.set_message_send_time(0.0)
+    message.set_message_arrival_time(1.1)
+    message.set_message_server_time(2.6)
+
+    event = Event(1, EventType.MSG_DEPT, 0.0, message)
+    assert get_time_and_node(event) == (2, 2.6)
+
+
+
