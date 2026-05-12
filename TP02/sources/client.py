4fc4fe00408c91695729dfe07cc31c82b99c48aa
gabibouty <gabi_bouty@hotmail.com>
2026-05-12 18:42:36 +0200

    Prepare Client class (and format test_trace)

---
+++ TP02/sources/client.py
@@ -0,0 +1,15 @@
+from typing import List
+from sources.events_and_messages import Message
+
+# TODO:
+#   - Id, List(Message)
+#
+#   - @Init ==> create message list
+#   - get_next_msg_time ()
+#   - pop_message ()
+#
+
+
+class Client:
+    def __init__(self):
+        pass
--- TP02/tests/test_trace.py
+++ TP02/tests/test_trace.py
@@ -3,6 +3,7 @@ from sources.trace import get_time_and_node, generateTraceOut, generateTraceCSV
 import csv
 import os
 
+
 def test_get_time_and_node_event_SEND_MSG():
     message = Message(1, 1, 2)
     message.set_message_send_time(0.0)
@@ -31,13 +32,14 @@ def test_get_time_and_node_event_MSG_DEPT():
 
     event = Event(1, EventType.MSG_DEPT, 0.0, message)
     assert get_time_and_node(event) == (2, 2.6)
-    
+
+
 def test_generateTraceOut_SEND_MSG(capsys):
     message1 = Message(1, 1, 2)
     message1.set_message_send_time(0.0)
     message1.set_message_arrival_time(1.1)
     message1.set_message_server_time(2.6)
-    
+
     message2 = Message(2, 1, 3)
     message2.set_message_send_time(0.2)
     message2.set_message_arrival_time(1.2)
@@ -46,22 +48,22 @@ def test_generateTraceOut_SEND_MSG(capsys):
     event1 = Event(1, EventType.SEND_MSG, 0.0, message1)
     event2 = Event(2, EventType.SEND_MSG, 0.0, message2)
     t_event = [event1, event2]
-    
+
     generateTraceOut(t_event)
-    
+
     # https://docs.pytest.org/en/6.2.x/capture.html
     capture = capsys.readouterr()
     assert f"time\tnode\tevent\tsrc\tdst\tmsgID" in capture.out
     assert f"0.0\t1\tSEND_MSG\t1\t2\t1" in capture.out
     assert f"0.2\t1\tSEND_MSG\t1\t3\t2" in capture.out
-    
-    
+
+
 def test_generateTraceOut_RECV_MSG(capsys):
     message1 = Message(42, 1, 2)
     message1.set_message_send_time(0.0)
     message1.set_message_arrival_time(1.1)
     message1.set_message_server_time(2.6)
-    
+
     message2 = Message(43, 1, 3)
     message2.set_message_send_time(0.2)
     message2.set_message_arrival_time(1.2)
@@ -70,21 +72,22 @@ def test_generateTraceOut_RECV_MSG(capsys):
     event1 = Event(1, EventType.RECV_MSG, 0.0, message1)
     event2 = Event(2, EventType.RECV_MSG, 0.0, message2)
     t_event = [event1, event2]
-    
+
     generateTraceOut(t_event)
-    
+
     # https://docs.pytest.org/en/6.2.x/capture.html
     capture = capsys.readouterr()
     assert f"time\tnode\tevent\tsrc\tdst\tmsgID" in capture.out
     assert f"1.1\t2\tRECV_MSG\t1\t2\t42" in capture.out
     assert f"1.2\t3\tRECV_MSG\t1\t3\t43" in capture.out
-    
+
+
 def test_generateTraceOut_MSG_DEPT(capsys):
     message1 = Message(42, 1, 2)
     message1.set_message_send_time(0.0)
     message1.set_message_arrival_time(1.1)
     message1.set_message_server_time(2.6)
-    
+
     message2 = Message(43, 1, 3)
     message2.set_message_send_time(0.2)
     message2.set_message_arrival_time(1.2)
@@ -93,23 +96,22 @@ def test_generateTraceOut_MSG_DEPT(capsys):
     event1 = Event(1, EventType.MSG_DEPT, 0.0, message1)
     event2 = Event(2, EventType.MSG_DEPT, 0.0, message2)
     t_event = [event1, event2]
-    
+
     generateTraceOut(t_event)
-    
+
     # https://docs.pytest.org/en/6.2.x/capture.html
     capture = capsys.readouterr()
     assert f"time\tnode\tevent\tsrc\tdst\tmsgID" in capture.out
     assert f"2.6\t2\tMSG_DEPT\t1\t2\t42" in capture.out
     assert f"4.6\t3\tMSG_DEPT\t1\t3\t43" in capture.out
-    
-    
-    
+
+
 def test_generateTraceCSV():
     message1 = Message(42, 1, 2)
     message1.set_message_send_time(0.0)
     message1.set_message_arrival_time(1.1)
     message1.set_message_server_time(2.6)
-    
+
     message2 = Message(43, 1, 3)
     message2.set_message_send_time(0.2)
     message2.set_message_arrival_time(1.2)
@@ -118,9 +120,9 @@ def test_generateTraceCSV():
     event1 = Event(1, EventType.SEND_MSG, 0.0, message1)
     event2 = Event(2, EventType.MSG_DEPT, 0.0, message2)
     t_event = [event1, event2]
-    
+
     generateTraceCSV(t_event)
-    
+
     # https://docs.python.org/3/library/csv.html
     with open("trace.csv", mode="r", newline="", encoding="utf-8") as csvfile:
         spamreader = csv.reader(csvfile, delimiter=";", quotechar='"')
