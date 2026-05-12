aa6683c3e03d1d145ab6b06bef0884214c3582a9
gabibouty <gabi_bouty@hotmail.com>
2026-05-12 20:00:42 +0200

    Add TODO for client

--- TP02/sources/client.py
+++ TP02/sources/client.py
@@ -12,14 +12,18 @@ from sources.events_and_messages import Message
 
 class Client:
     def __init__(self, t_id: int, t_messages: List[Message]):
+        # TODO: generate Message list directly in Client __init__
         self.__id = t_id
         self.__messages = t_messages
 
     def get_client_id(self) -> int:
         return self.__id
 
+    # TODO: remove this
     def get_messages(self) -> List[Message]:
         return self.__messages
+
+    # TODO: add a method to check if there is message
 
     def get_next_msg_time(self) -> float:
         return self.__messages[0].get_message_send_time()
